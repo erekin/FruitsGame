8e10f574d8dea30ac351baf6ba4dd0f865a0b5f0
erekin <ereki7@gmail.com>
2023-11-16 01:20:04 +0900

    一方だけを変化（マージ）させるシステム完成！！

--- Assets/Scripts/FruitsController.cs
+++ Assets/Scripts/FruitsController.cs
@@ -5,38 +5,28 @@ using UnityEngine;
 public class FruitsController : MonoBehaviour
 {
     [SerializeField] Camera _cam;
-    public MainManager mainManager;
 
-    private Vector3 offset;
+    private MainManager mainManager;
     private float yPos_fixed;   //y座標を固定
     private Rigidbody2D rb;
-    public int id;
+    public bool isMerge;
 
     private void Start()
     {
+        mainManager = GameObject.FindWithTag("MainManager").GetComponent<MainManager>();
         //y座標を固定
         yPos_fixed = gameObject.transform.position.y;
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0;
     }
 
-    private void OnMouseDown()
-    {    
-        offset = gameObject.transform.position - GetMousePos();
-    }
-
     private void OnMouseDrag()
     {
-        transform.position = GetMousePos() + offset ;
+        transform.position = (Vector2)_cam.ScreenToWorldPoint(Input.mousePosition);
         //y座標のドラッグの値を固定
         Vector3 pos = transform.position;
         pos.y = yPos_fixed;
         transform.position = pos;
-    }
-
-    private Vector3 GetMousePos()
-    {
-        return _cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void OnMouseUp()
@@ -50,29 +40,18 @@ public class FruitsController : MonoBehaviour
         mainManager.FruitsGenerate();
     }
 
-    private bool isMerge = false;
-    private  void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject colObj = collision.gameObject;
-
-        if (colObj.gameObject.CompareTag("fruits"))
+        if (colObj.CompareTag("fruits"))
         {
             FruitsController colFruits = collision.gameObject.GetComponent<FruitsController>();
-            if (id == colFruits.id &&
-                     !isMerge &&
-                     !colFruits.isMerge &&
-                     id < mainManager.MaxFruitsNo - 1){
-                isMerge = true;
-                colFruits.isMerge = true;
-                mainManager.MergeNext(transform.position, id);
-                Destroy(gameObject);
-                Destroy(colFruits.gameObject);
+            if(gameObject.name == colFruits.name && !isMerge && !colFruits.isMerge)
+            {
+                Debug.Log("マージ！！");
             }
+            isMerge = true;
+            colFruits.isMerge = true;
         }
-
-        //if(collision.gameObject.CompareTag("blank"))
-        //{
-        //    mainManager.TouchFruits(id,this.gameObject);
-        //}
     }
 }
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,60 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using System;
 
 public class MainManager : MonoBehaviour
 {
-    [SerializeField] private Vector2 basePos;
-    [SerializeField] private Transform firstPos;
-    [SerializeField] private FruitsController[] fruits;
+    [SerializeField] private Transform basePos;
+    [SerializeField] private GameObject[] fruits;
   
     private bool firstFruits;
-    public int MaxFruitsNo { get; private set; }
+    private GameObject generateFruits;
+    private int MaxFruitsNo;
 
     void Start()
     {
-        //firstFruits = true;
-        //generateFruits = fruits[0];
-
+        firstFruits = true;
+        generateFruits = fruits[0];
         MaxFruitsNo = fruits.Length;
         FruitsGenerate();
     }
 
-    void Update()
-    {
-
-    }
-
     public void FruitsGenerate()
     {
-        int i = Random.Range(0, MaxFruitsNo - 2);
-        Debug.Log(i);
-        FruitsController fruitsIn = Instantiate(fruits[i], firstPos);
-        fruitsIn.id = i;
-        fruitsIn.gameObject.SetActive(true);
-        //firstFruits = false;
+        if (!firstFruits)
+        {
+            int i = Random.Range(0, MaxFruitsNo -1);
+            generateFruits = fruits[i];
+        }
+        
+        Instantiate(generateFruits, (Vector2)basePos.position,Quaternion.identity);
+        firstFruits = false;
     }
 
-    //private GameObject changeObj;
-    private bool isMerge = false;
-    //private void ChangeFruits(int id,GameObject obj)
-    //{
-    //    id++;
-    //    changeObj = fruits[id];
-    //    if (!isMerge)
-    //    {
-    //        Instantiate(changeObj, obj.transform.position, Quaternion.identity);
-    //        isMerge = true;
-    //    }
-
-    //}
-
-    public void MergeNext(Vector3 target,int id)
-    {
-        FruitsController fruitsIns = Instantiate(fruits[id + 1], target, Quaternion.identity, firstPos);
-        fruitsIns.id = id + 1;
-        fruitsIns.GetComponent<Rigidbody2D>().gravityScale = 2.0f;
-
-    }
 }
